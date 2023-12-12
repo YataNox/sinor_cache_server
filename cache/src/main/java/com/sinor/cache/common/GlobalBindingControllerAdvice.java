ff6ad8c80d27cd38bbf93affa5593c22530f8aaa
Asac_Full <rladnwls0523@naver.com>
2023-12-12 14:01:23 +0900

    feat[exception_handler] : Exception Handling 추가
    Internal Server Error Exception Handler Method 작성

--- cache/src/main/java/com/sinor/cache/common/GlobalBindingControllerAdvice.java
+++ cache/src/main/java/com/sinor/cache/common/GlobalBindingControllerAdvice.java
@@ -2,10 +2,13 @@ package com.sinor.cache.common;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,9 +16,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestControllerAdvice
 public class GlobalBindingControllerAdvice {
+	/**
+	 * CustomException Handler
+	 */
 	@ExceptionHandler(CustomException.class)
 	public ResponseEntity<?> customException(CustomException e){
-		log.error("Base exception occurred" + e.getStatus().getCode(), e);
+		log.error("Base exception occurred : " + e.getStatus().getCode(), e);
 
 		Map<String, Object> responseBody = new HashMap<>();
 		responseBody.put("timestamp", LocalDateTime.now());
@@ -24,5 +30,22 @@ public class GlobalBindingControllerAdvice {
 		responseBody.put("message", e.getStatus().getMessage());
 
 		return ResponseEntity.status((e).getStatus().getCode()).body(responseBody);
+	}
+
+	/**
+	 * Internal Server Error Exception Handler
+	 */
+	@ExceptionHandler(Exception.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public ResponseEntity<?> Exception(Exception e){
+		log.error("Internal Server Error occurred : " + HttpStatus.INTERNAL_SERVER_ERROR.value(), e);
+
+		Map<String, Object> responseBody = new LinkedHashMap<>();
+		responseBody.put("timestamp", LocalDateTime.now());
+		responseBody.put("isSuccess", false);
+		responseBody.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		responseBody.put("message", "Internal Server Error occurred");
+
+		return ResponseEntity.internalServerError().body(responseBody);
 	}
 }
