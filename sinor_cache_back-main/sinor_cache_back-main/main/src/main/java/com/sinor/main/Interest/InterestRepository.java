6d9b5b3d936d68695faa1052bd4fca71b7ac8acb
YataNox <65604233+YataNox@users.noreply.github.com>
2024-01-11 10:45:11 +0900

    Delete sinor_cache_back-main/sinor_cache_back-main directory

--- sinor_cache_back-main/sinor_cache_back-main/main/src/main/java/com/sinor/main/Interest/InterestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sinor.main.Interest;
-
-
-import com.sinor.main.Interest.model.Interest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InterestRepository extends JpaRepository<Interest,Long> {
-}
--- sinor_cache_back-main/sinor_cache_back-main/main/src/main/java/com/sinor/main/gatheringcontent/GatheringContentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sinor.main.gatheringcontent;
-
-
-import com.sinor.main.gatheringcontent.model.GatheringContent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GatheringContentRepository extends JpaRepository<GatheringContent,Long> {
-}
